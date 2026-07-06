f9aaef6c8e142bf9cdcd0526a69da90baff826f5
Baptistegtv <165500080+Baptistegtv@users.noreply.github.com>
2026-07-06 14:40:40 +0200

    Add ground.h header for ground vertices definition

---
+++ source/ground.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "vertex.h"
+
+// Un grand quad plat (2 triangles) représentant le sol herbeux de l'île de départ
+extern const vertex groundVertices[6];
+extern const int groundVertexCount;
