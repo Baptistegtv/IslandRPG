d0c14f51b2026e54964b8479f8be900ce579619d
Baptistegtv <165500080+Baptistegtv@users.noreply.github.com>
2026-07-07 13:21:39 +0200

    Replace vshader_shbin.h with shader_shbin.h

--- source/main.c
+++ source/main.c
@@ -8,7 +8,7 @@
 #include "player.h"
 #include "camera.h"
 #include "ground.h"
-#include "vshader_shbin.h"
+#include "shader_shbin.h"
 
 #define DISPLAY_TRANSFER_FLAGS \
 	(GX_TRANSFER_FLIP_VERT(0) | GX_TRANSFER_OUT_TILED(0) | GX_TRANSFER_RAW_COPY(0) | \
